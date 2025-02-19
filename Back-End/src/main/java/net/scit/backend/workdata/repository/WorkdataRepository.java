497612935a98509283fccfa0d1ab43b50a6b4035
jasperPark11 <pkn1999199@naver.com>
2025-02-19 16:31:05 +0900

    Revert "feat/ workdata Create complete"

--- Back-End/src/main/java/net/scit/backend/workdata/repository/WorkdataRepository.java
+++
@@ -1,8 +0,0 @@
-package net.scit.backend.workdata.repository;
-
-import net.scit.backend.workdata.entity.WorkdataEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkdataRepository extends JpaRepository<WorkdataEntity, Long> {
-
-}
