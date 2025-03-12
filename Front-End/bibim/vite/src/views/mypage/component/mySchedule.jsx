2f4fdaf086c33b370726b5963a8e71e1560ef965
KimSebin <saeb0325@gmail.com>
2025-03-12 17:44:13 +0900

    시작

---
+++ Front-End/bibim/vite/src/views/mypage/component/mySchedule.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MySchedule = () => {
+  return (
+    <div>
+      <h2>My Schedule</h2>
+      {/* 여기에 스케줄 관련 내용을 추가하세요 */}
+    </div>
+  );
+};
+
+export default MySchedule; 
--- Front-End/bibim/vite/src/views/mypage/index.jsx
+++ Front-End/bibim/vite/src/views/mypage/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import MyInfo from './component/myInfo'; 
+import MySchedule from './component/mySchedule';
 
 const MyPage = () => {
   return (
-    <MyInfo />
+    <>
+      <MyInfo />
+      <MySchedule />
+    </>
   );
 };
 
