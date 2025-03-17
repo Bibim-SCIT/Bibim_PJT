a1911eee1ab4c1ca2085c0e0b4a83c4f22eec081
sssungjooon <dellojoon7@gmail.com>
2025-03-17 12:48:38 +0900

    feat: 라우터 수정 및 랜딩 페이지 바로 렌더링

--- Front-End/bibim/vite/src/routes/MainRoutes.jsx
+++ Front-End/bibim/vite/src/routes/MainRoutes.jsx
@@ -6,7 +6,7 @@ import Loadable from 'ui-component/Loadable';
 // import WorkDataPage from '../views/workdata';
 
 // dashboard routing
-const DashboardDefault = Loadable(lazy(() => import('views/dashboard/Default')));
+// const DashboardDefault = Loadable(lazy(() => import('views/dashboard/Default')));
 
 // sample page routing
 const DMPage = Loadable(lazy(() => import('views/dm')));
@@ -29,23 +29,23 @@ const MainRoutes = {
   path: '/',
   element: <MainLayout />,
   children: [
-    {
-      path: 'dashboard',
-      children: [
-        {
-          path: 'default',
-          element: <DashboardDefault />
-        }
-      ]
-    },
+    // {
+    //   path: 'dashboard',
+    //   children: [
+    //     {
+    //       path: 'default',
+    //       element: <DashboardDefault />
+    //     }
+    //   ]
+    // },
     {
       path: '/dm',
       element: <DMPage />
     },
-    {
-      path: '/meeting',
-      element: <MeetingPage />
-    },
+    // {
+    //   path: '/meeting',
+    //   element: <MeetingPage />
+    // },
     {
       path: '/workdata',
       element: <WorkDataPage />
